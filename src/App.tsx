625f897c81c96cbde9c0b89988813a52e6b6fb74
Tam Nguyen <tam.nguyen@piksal.com>
2020-10-04 23:41:04 +0700

    add: package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+import logo from "./logo.svg";
+import "./App.scss";
+
+const Title = styled.div``;
 
 function App() {
   return (
-    <div className="App">
+    <Title className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +23,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Title>
   );
 }
 
